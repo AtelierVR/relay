70ffe17782a1ce92caddf96376b5b893a806f7cb
hactazia <hactazia@gmail.com>
2025-09-17 19:59:03 +0200

    refactor: improve code readability and logging in SegmentationHandler

--- src/Requests/Segmentation/SegmentationHandler.cs
+++ src/Requests/Segmentation/SegmentationHandler.cs
@@ -5,35 +5,40 @@ using Relay.Priority;
 
 namespace Relay.Requests.Segmentation;
 
-public class SegmentationHandler : Handler {
+public class SegmentationHandler : Handler
+{
 	private static readonly ConcurrentDictionary<int, SegmentedPacket> Segmented = new();
 	private static readonly Timer CleanupTimer;
 
-	static SegmentationHandler() {
+	static SegmentationHandler()
+	{
 		// Initialize cleanup timer to run every 10 seconds
 		CleanupTimer = new Timer(CleanupExpiredPackets, null, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10));
 	}
 
-	private static void CleanupExpiredPackets(object? state) {
+	private static void CleanupExpiredPackets(object? state)
+	{
 		var config = Config.Load();
-		var timeout = TimeSpan.FromSeconds(config.GetSegmentationTimeout());
+		var timeout = config.GetSegmentationTimeout();
 		var expiredKeys = new List<int>();
 
-		foreach (var kvp in Segmented) 
-			if (kvp.Value.IsExpired(timeout)) 
+		foreach (var kvp in Segmented)
+			if (kvp.Value.IsExpired(timeout))
 				expiredKeys.Add(kvp.Key);
-			
-		foreach (var key in expiredKeys) 
-			if (Segmented.TryRemove(key, out _)) 
-				Logger.Info($"Removed expired segmented packet with key {key}");
+
+		foreach (var key in expiredKeys)
+			if (Segmented.TryRemove(key, out _))
+				Logger.Warning($"Removed expired segmented packet with key {key}");
 	}
 
-	protected override void OnSetup() {
+	protected override void OnSetup()
+	{
 		PacketPriorityManager.SetMinimumPriority(RequestType.Segmentation, EPriority.Critical);
 		PacketDispatcher.RegisterHandler(RequestType.Segmentation, OnSegmentation);
 	}
 
-	private static void OnSegmentation(PacketData data) {
+	private static void OnSegmentation(PacketData data)
+	{
 		var sid = data.Payload.ReadUShort();
 		var all = data.Payload.ReadUShort();
 		var crt = data.Payload.ReadUShort();
@@ -41,11 +46,18 @@ public class SegmentationHandler : Handler {
 
 		var key = HashCode.Combine(sid, data.Client.Remote.GetHashCode());
 
-		if (!Segmented.TryGetValue(key, out var packet)) {
+		if (!Segmented.TryGetValue(key, out var packet))
+		{
 			packet = new SegmentedPacket(all);
 			Segmented[key] = packet;
-		} else if (packet.IsExpired(timeout))
+		}
+		else
+		{
+			var config = Config.Load();
+			var timeout = config.GetSegmentationTimeout();
+			if (packet.IsExpired(timeout))
 				return;
+		}
 
 		packet.AddSegment(crt, spl);
 
