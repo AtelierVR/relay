7b044ed9449026b39250d0fb8303078bd06ff05a
hactazia <hactazia@gmail.com>
2026-03-04 19:09:13 +0000

    refactor: Enhance PlayerStatus enum by adding Default trait and removing redundant Default implementation

--- src/player/player_status.rs
+++ src/player/player_status.rs
@@ -1,18 +1,13 @@
 /// Current lifecycle status of a player in an instance.
 #[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum PlayerStatus {
+    #[default]
     None = 0,
     NeedPassword = 1,
     Preparing = 2,
     Traveling = 3,
     Ready = 4,
-}
-
-impl Default for PlayerStatus {
-    fn default() -> Self {
-        Self::None
-    }
 }
 
 impl TryFrom<u8> for PlayerStatus {
